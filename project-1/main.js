208f63389eafb3e06694f6dc810098b2faa22612
John You <blvckyou@yahoo.com>
2017-09-15 14:25:18 -0700

    Updated getbeltDetails function and created a 'img' element and setAttribute to 'src, data.img'

--- project-1/main.js
+++ project-1/main.js
@@ -2,16 +2,19 @@
 var belts = [
   {
     id: '1',
+    img: 'belts/dualprong.jpg',
     name:'Dual Prong Belt',
     details: 'Zinc plated steel roller buckle. Six rows of lock-stiched, corrosive resistant, hi-density nylon.  Finished with top quality, fine suade providing non-slip surface. One solid thickness of the finest, and select leather.'
   },
   {
     id: '2',
+    img: 'belts/singleprong.jpg',
     name: 'Single Prong Belt',
     details: 'Zinc plated steel roller buckle. Four rows of lock-stiched, corrosive resistant, hi density nylon. Closer prong holes for more choice in precise fitting.'
   },
   {
     id: '3',
+    img: 'belts/leverbelt.jpg',
     name: 'Lever Belt',
     details: 'Lever makes atheletes tighten and secure themselves with the push of the Lever. Also can be loosened in less than a second with a quick flick of the patented Lever. Four rows of lock-stiched, corrosive resistant, hi density nylon.'
   }
@@ -37,6 +40,9 @@ function getbeltDetails(data) {
   $name.textContent = data.name
   var $description = document.createElement('p')
   $description.textContent = data.details
+  var $img = document.createElement('img')
+  $img.setAttribute('src', data.img)
+  $info.appendChild($img)
   $info.appendChild($name)
   $info.appendChild($description)
   $info.style.border = '1px solid grey'
