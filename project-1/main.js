87ba10b0572fb8e7310fd7bd3e14fe99ac4b195a
John You <blvckyou@yahoo.com>
2017-09-15 13:27:27 -0700

    updates

--- project-1/main.js
+++ project-1/main.js
@@ -1,20 +1,4 @@
 /* eslint-disable no-unused-vars */
-
-/*
-var $img1 = document.querySelector('#img1')
-var $img2 = document.querySelector('#img2')
-
-function imgMouseOver() {
-  $img2.style.display = 'block'
-}
-function imgMouseOut() {
-  $img2.style.display = 'none'
-}
-$img1.addEventListener('mouseover', imgMouseOver)
-$img2.addEventListener('mouseout', imgMouseOut)
-
-*/
-
 var belts = [
   {
     id: '1',
